18dfed6562aa6f46aeee86dbdf73e51cbb64531b
Vamsi1656 <vamsireddy1717@gmail.com>
2023-05-18 16:22:04 +0530

    first commit

---
+++ one.js
@@ -0,0 +1,24 @@
+/*Write a program to print 1 to 10 numbers
+for(i=0;i<=10;i++){
+    console.log(i)
+}*/
+/*write a program to print 10 to 1 numbers
+for (i=10;i>=1;i--){
+    console.log(i)
+}*/
+/*write a program to print 5 to 15 numbers
+for(i=5;i<=15;i++){
+    console.log(i)
+}*/
+/*write a program to print 15 to 10 numbers
+for(i=15;i>=10;i--){
+    console.log(i)
+}*/
+/*write a program to print first 10 even numbers 
+for(i=0;i<20;i=i+2){
+    console.log(i)
+}*/
+/*write a program to print first 10 odd numbers */
+for(i=1;i<20;i=i+2){
+    console.log(i)
+}
